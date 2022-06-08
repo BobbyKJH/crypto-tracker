e7bdd14a6993a2e5f872cf17aaaef1d5de47049e
BobbyKJH <rlatnguscjsw@gmail.com>
2022-06-08 13:27:55 +0900

    start coin

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
-export const lightTheme: DefaultTheme = {
+export const theme: DefaultTheme = {
   bgColor: "#fff",
   textColor: "#000",
   btnColor: "#fff",
-};
-
-export const darkTheme: DefaultTheme = {
-  bgColor: "#000",
-  textColor: "#fff",
-  btnColor: "aqua",
 };
